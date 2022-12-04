b5cf023251c98f3ad144683ebb1006bea5a60bb2
Martin Billinger <mbillingr@users.noreply.github.com>
2022-12-04 21:18:02 +0100

    day 4 - part 2

--- src/day04.cr
+++ src/day04.cr
@@ -19,21 +19,32 @@ class Part1 < Day
   @part = "Part 1"
 
   def solve(data)
-    data.select { |pair| fully_overlaps?(pair[0], pair[1]) }
+    data
+      .select do |pair|
+        fully_overlaps?(pair[0], pair[1]) || fully_overlaps?(pair[1], pair[0])
+      end
       .size
   end
-end
-
-def fully_overlaps?(rng1, rng2)
-  rng1[0] >= rng2[0] && rng1[1] <= rng2[1] || rng2[0] >= rng1[0] && rng2[1] <= rng1[1]
 end
 
 class Part2 < Day
   @part = "Part 2"
 
   def solve(data)
-    puts data
+    data
+      .select do |pair|
+        overlaps?(pair[0], pair[1]) || overlaps?(pair[1], pair[0])
+      end
+      .size
   end
+end
+
+def fully_overlaps?(rng1, rng2)
+  rng1[0] >= rng2[0] && rng1[1] <= rng2[1]
+end
+
+def overlaps?(rng1, rng2)
+  rng1[0] <= rng2[1] && rng1[1] >= rng2[1] || rng1[0] <= rng2[0] && rng1[1] >= rng2[0]
 end
 
 raw_data = File.read(__DIR__ + "/../data/input#{DAY}.txt")
@@ -41,7 +52,7 @@ raw_data = File.read(__DIR__ + "/../data/input#{DAY}.txt")
 Part1.new.check(EXAMPLE, 2)
 Part1.new.run(raw_data)
 
-Part2.new.check(EXAMPLE, "expected")
+Part2.new.check(EXAMPLE, 4)
 Part2.new.run(raw_data)
 
 EXAMPLE = "2-4,6-8
