300dc59cb54c290d3818c3095bcc20f6e8b80200
Martin Billinger <mbillingr@users.noreply.github.com>
2022-12-21 09:57:25 +0100

    day 21 - part 2 in python
    too painful in crystal

--- src/day21.py
+++ src/day21.py
@@ -4,10 +4,18 @@ from utils.puzzle import Puzzle
 from utils import parsing as p
 
 NUMBER = p.Number()
-EXPR = p.String(p.Letter() * ...) + p.SkipWhitespace() + p.OneOf("+-*/") + p.SkipWhitespace() + p.String(p.Letter() * ...)
+EXPR = (
+    p.String(p.Letter() * ...)
+    + p.SkipWhitespace()
+    + p.OneOf("+-*/")
+    + p.SkipWhitespace()
+    + p.String(p.Letter() * ...)
+)
 
 PARSER = p.SeparatedList(
-    p.Group(p.String(p.Letter() * ...) + p.Drop(":") + p.SkipWhitespace() + (NUMBER | EXPR)),
+    p.Group(
+        p.String(p.Letter() * ...) + p.Drop(":") + p.SkipWhitespace() + (NUMBER | EXPR)
+    ),
     p.Str("\n"),
 )
 
@@ -31,7 +39,79 @@ class Part2(Day00):
         super().__init__("2")
 
     def solve(self, data):
-        print(data)
+        del data["humn"]
+
+        lhs = build_expression(data["root"][1], data)
+        rhs = build_expression(data["root"][2], data)
+
+        lhs = partial_eval(lhs)
+        rhs = partial_eval(rhs)
+
+        return solve_equal(lhs, rhs)
+
+
+def solve_equal(lhs, rhs):
+    match (lhs, rhs):
+        case str(), int():
+            return rhs
+
+        case ("/", a, int(b)), int():
+            return solve_equal(a, rhs * b)
+
+        case ("+", int(a), b), int():
+            return solve_equal(b, rhs - a)
+        case ("+", a, int(b)), int():
+            return solve_equal(a, rhs - b)
+
+        case ("-", a, int(b)), int():
+            return solve_equal(a, rhs + b)
+        case ("-", int(a), b), int():
+            return solve_equal(b, a - rhs)
+
+        case ("*", int(a), b), int():
+            return solve_equal(b, rhs // a)
+        case ("*", a, int(b)), int():
+            return solve_equal(a, rhs // b)
+
+        case _:
+            raise NotImplementedError(f"{lhs} = {rhs}")
+
+
+def build_expression(x, data):
+    match x:
+        case str():
+            if x not in data:
+                return x
+            else:
+                return build_expression(data[x], data)
+        case int():
+            return x
+        case (op, a, b):
+            return (
+                op,
+                build_expression(a, data),
+                build_expression(b, data),
+            )
+
+
+def partial_eval(exp):
+    match exp:
+        case (op, a, b):
+            a = partial_eval(a)
+            b = partial_eval(b)
+            match (op, a, b):
+                case ("+", int(), int()):
+                    return a + b
+                case ("-", int(), int()):
+                    return a - b
+                case ("*", int(), int()):
+                    return a * b
+                case ("/", int(), int()):
+                    return a // b
+                case other:
+                    return other
+        case _:
+            return exp
 
 
 EXAMPLE = """root: pppw + sjmn
@@ -50,7 +130,6 @@ lgvd: ljgn * ptdq
 drzm: hmdt - zczc
 hmdt: 32
 """
-
 
 
 Part2().check(EXAMPLE, 301)
